43b10259cb9a837479b4b2bf8694e7aaa008826f
Mbuka1 <mbukakennedy@example.com>
2022-09-12 13:06:23 -0700

    Islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "betty.h"
+#include<stdlib.h>
+/**
+*Function that checks for lowercase character
+*Prototype: int _islower(int c)
+*Returns 1 if c is lowercase
+*Returns 0 otherwise
+*/
+int _islower(int c)
+{char c;
+if (c > 96 && c < 122)
+return (1);
+else
+return (0);
+}
