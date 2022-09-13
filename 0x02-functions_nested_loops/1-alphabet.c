42b044cb526b8d90d6607a190809827ca8ed39c9
Kennedy Ombajo <111004292+Mbuka1@users.noreply.github.com>
2022-09-13 12:10:45 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,16 @@
-#include<main.h>
+#include "betty.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_alphabet - Print alphabet in lowercase using only `_putchar`
+ * Return: 0
  */
-int main(void)
+void print_alphabet(void)
 {
-    print_alphabet();
-    return (0);
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
 }
