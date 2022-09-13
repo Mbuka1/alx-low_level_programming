27ed65ea419742f5931e2ae18efdec66a4ed35ed
Kennedy Ombajo <111004292+Mbuka1@users.noreply.github.com>
2022-09-13 19:37:16 +0300

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,8 @@
 #include "main.h"
-#include "betty-style.pl"
-
 /**
  * add - Add two integers
- * a: int type number
- * b: int type number
+ * @a: int type number
+ * @b: int type number
  * Return: result of addition
  */
 int add(int, int)
