4a03c637f8415ba42cc3fcfc8e51dabd791280d8
Kennedy Ombajo <111004292+Mbuka1@users.noreply.github.com>
2022-09-13 12:20:29 +0300

    Update betty.h

--- 0x02-functions_nested_loops/betty.h
+++ 0x02-functions_nested_loops/betty.h
@@ -1,3 +1,6 @@
+#ifndef BETTY_H
+#define BETTY_H
+
 int _putchar(char c);
 int _isupper(int c);
 int _islower(int c);
@@ -11,3 +14,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+
+#endif /* BETTY_H */
