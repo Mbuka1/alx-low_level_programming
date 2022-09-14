526cdf94852be9761c84eca5e075eb61e482296c
Kennedy Ombajo <111004292+Mbuka1@users.noreply.github.com>
2022-09-14 20:22:30 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
 * main - tests function that prints if integer is positive or negative
@@ -13,5 +13,5 @@ int main(void)
         i = 98;
         positive_or_negative(i);
 
-	return (0);
+        return (0);
 }
