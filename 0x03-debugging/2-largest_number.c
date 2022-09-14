c91ebd4621b0b6e732949158e497f48c980693a0
Kennedy Ombajo <111004292+Mbuka1@users.noreply.github.com>
2022-09-14 21:01:45 +0300

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -10,21 +9,20 @@
  */
 
 int largest_number(int a, int b, int c)
-{
-    int largest;
+{int largest;
 
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
+ if (a > b && b > c)
+ {
+ largest = a;
+ }
+ else if (b > a && a > c)
+ {
+ largest = b;
+ }
+ else
+ {
+ largest = c;
+ }
 
-    return (largest);
+return (largest);
 }
