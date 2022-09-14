e85e5590dc6ebbea9ebf490f4ada14aaa71dbc2a
Kennedy Ombajo <111004292+Mbuka1@users.noreply.github.com>
2022-09-14 20:07:29 +0300

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -11,3 +13,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 void positive_or_negative(int i);
+#endif /* MAIN_H */
