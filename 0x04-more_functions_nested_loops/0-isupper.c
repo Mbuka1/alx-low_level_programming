08b822d44bef833c6b0052f24a1ea9d325b22c81
Kennedy Ombajo <111004292+Mbuka1@users.noreply.github.com>
2022-09-15 16:04:51 +0300

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,11 +8,14 @@
  */
 int main(void)
 {
-    char c;
-
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
+char c;
+if (c = 'A')
+{
+printf("%c: %d\n", c, _isupper(c));
+retrun (1);
+}
+else (c = 'a'){
+printf("%c: %d\n", c, _isupper(c));
+return (0);
+}
 }
