1185c21961cbbba7d48daa850021bc8b21e72129
Kennedy Ombajo <111004292+Mbuka1@users.noreply.github.com>
2022-09-15 16:36:26 +0300

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,6 +6,7 @@ int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 int _putchar(char c);
+void print_numbers(void)
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
