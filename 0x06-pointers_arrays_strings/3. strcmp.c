bfde64cc2124e29988c195a7d8122ec8a0809b1b
Kennedy Ombajo <111004292+Mbuka1@users.noreply.github.com>
2022-09-22 17:55:11 +0300

    3. strcmp

--- 0x06-pointers_arrays_strings/3. strcmp.c
+++ 0x06-pointers_arrays_strings/3. strcmp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include<stdio.h>
 /**
  * _strcmp - Compare two strings
  * @s1: string
@@ -8,12 +7,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
-	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-	}
-	return (0);
+int i;
+for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+{
+if (s1[i] != s2[i])
+return (s1[i] - s2[i]);
+}
+return (0);
 }
