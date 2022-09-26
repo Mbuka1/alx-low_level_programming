66751e69e25800de2396666e0cd12c600e7e17b7
Kennedy Ombajo <111004292+Mbuka1@users.noreply.github.com>
2022-09-26 20:39:30 +0300

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,14 +7,14 @@
  */
 char *_strchr(char *s, char c)
 {
-while (*s != '\0')
-{
-if (*s == c)
-return (s);
-elseif (*(s+1) b== c)
-return (s+1);
-s++;
-}
-return (s+1);
-}
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		else if (*(s + 1) == c)
+			return (s + 1);
+		s++;
+	}
 
+	return (s + 1);
+}
