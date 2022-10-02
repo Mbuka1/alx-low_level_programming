d39b2734aedb330bea182490fa83c481f310ff01
Kennedy Ombajo <111004292+Mbuka1@users.noreply.github.com>
2022-10-02 09:47:40 +0300

    my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,12 +7,11 @@
  *    * @argv: arguments passed to main as an array of strings
  *     * Return: 0
  *      */
-
 int main(int argc, char *argv[])
 {
-		(void) argc;
+	(void) argc;
 
-			printf("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 
-				return (0);
+	return (0);
 }
